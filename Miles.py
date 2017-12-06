007c3c5bbaef40c480b58d67e1630f324c6637e9
Agata Korzeniowska <akorzen1@interia.pl>
2017-12-06 11:59:27 +0100

    Edytowałam plik Miles.py

--- Miles.py
+++ Miles.py
@@ -6,6 +6,5 @@ kilometers = input("Enter value in kilometers")
 # conversion factor
 conv_fac = 0.621371
 
-# calculate miles
 miles = kilometers * conv_fac
 print('%0.3f kilometers is equal to %0.3f miles' %(kilometers,miles))
