7371687b2a75298150b6dfd2c22ccda18deb5ba5
Agata Korzeniowska <akorzen1@interia.pl>
2017-12-06 12:05:10 +0100

    Zmieniłam plik Deck.py

--- Deck.py
+++ Deck.py
@@ -6,7 +6,6 @@ import itertools, random
 # make a deck of cards
 deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
 
-# shuffle the cards
 random.shuffle(deck)
 
 # draw five cards
