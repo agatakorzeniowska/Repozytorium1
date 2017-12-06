cf12000a4aa27dd915aac157420b278cd3c31efe
Agata Korzeniowska <akorzen1@interia.pl>
2017-12-06 12:05:29 +0100

    Zmieniłam plik Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -6,9 +6,6 @@ import calendar
 yy = 2014
 mm = 11
 
-# To ask month and year from the user
-# yy = int(input("Enter year: "))
-# mm = int(input("Enter month: "))
 
 # display the calendar
 print(calendar.month(yy, mm))
